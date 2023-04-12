a58658e60beb9a2db7bf21b929b452f934edbb3a
Luís Felipe <prof.luifel@gmail.com>
2023-04-12 15:49:11 -0300

    instalando o mongo

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express, {json} from "express";
-import cors from "cors"
+import cors from "cors";
 
 const PORT = 5000;
 const server = express();
 server.use(cors());
 server.use(json());
 
-
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 
 
