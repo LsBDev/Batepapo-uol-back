28e983a6573096a285dbc3c9b96a220bda2dbe47
Luís Felipe <prof.luifel@gmail.com>
2023-04-13 13:52:42 -0300

    Requisito: POST /participants

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
 import express, {json} from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
 
 const PORT = 5000;
-const server = express();
-server.use(cors());
-server.use(json());
+const app = express();
+app.use(cors());
+app.use(json());
+//app.use(express.json())
+dotenv.config();
 
+
+//conexão com o Banco de dados (que é uma aplicação separada do back que por sua vez é separada do front)
+let db;
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
+mongoClient.connect()
+    .then(() => db = mongoClient.db()) //requisição deu certo, coloco as infos do DB na variável db.
+    .catch((err) => console.log(err.message));
+
+app.post("/participants", (req, res) => {
+    const {name} = req.body;
+    const body = {name, lastStatus: Date.now()};
+    const exist = db.collection("participants").findOne({name: name});
+    if(!name) {
+        return res.status(422).send("Campo nome incorreto, digite um nome válido!");
+    } else if(exist) {
+        return res.status(409).send("Este nome já está sendo usado, escolha outro!");
+    } else {
+        db.collection("participants").insertOne(body)
+            .then(() => {
+                db.collection("messages".insertOne({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: Date.now()}))
+                    .then(() => res.send(201))
+                    .catch((err) => err.message)
+            })
+            .catch((err) => console.log(err.message));
+    }
+
+})
 
 
 
@@ -14,4 +44,4 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 
 
-server.listen(PORT, `Rodando na porta: ${PORT}`);
+app.listen(PORT, () => console.log(`Rodando na porta: ${PORT}`));
