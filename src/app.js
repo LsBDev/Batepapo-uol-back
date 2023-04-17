737400cbb542dd0e1a16cc9a26fb402a98b49432
Luís Felipe <prof.luifel@gmail.com>
2023-04-17 14:43:16 -0300

    ajuste código

--- src/app.js
+++ src/app.js
@@ -95,7 +95,7 @@ app.get("/participants", async (req, res) => {
 app.post("/messages", async (req, res) => {
     const {to, text, type} = req.body;
     const recMessage = {to: to, text: text, type: type};
-    const {user} = req.header;
+    const {user} = req.headers;
     const messageSchema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
@@ -127,7 +127,7 @@ app.post("/messages", async (req, res) => {
 
 //Função de GET Mensagens
 app.get("/messages", async (req, res) => {
-    const { user } = req.header;
+    const { user } = req.headers;
     const limit = req.query.limit;
     const numberLimit = parseInt(limit);
 
